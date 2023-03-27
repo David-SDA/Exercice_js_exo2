d22dc33ffb1d55963d2f90907463dd57c106f3a5
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-03-27 11:47:35 +0200

    Ajout de la base du javascript

---
+++ script.js
@@ -0,0 +1,15 @@
+function addClass(){
+
+}
+
+function removeClass(){
+
+}
+
+function clique(){
+
+}
+
+let lesCarres = document.querySelector(".carre");
+
+lesCarres.addEventListener("click", clique());
