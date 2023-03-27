f19f906f39869e24d0fb0bbce509336ade9b9304
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-03-27 13:52:53 +0200

    Correction et ajout évènement faire tourner carré

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
+/* Fonction pour ajouter une classe à un élément */
 function addClass(element, classe){
     element.classList.add(classe);
 }
 
+/* Fonction pour enlever une classe à un élément */
 function removeClass(element, classe){
     element.classList.remove(classe);
 }
 
-function clique(){
-    addClass(carre, "tourne");
-}
-
+/* Création d'un carré */
 const carre = document.createElement("div");
 carre.classList.add("carre");
 
-let contenu = document.querySelector(".contenu");
+/* On récupère le conteneur de tout les carré */
+const contenu = document.querySelector(".contenu");
 
 for(i = 1; i <= 4; i++){
-    let nouvCarre = carre.cloneNode();
-    nouvCarre.innerText = i;
-    contenu.appendChild(nouvCarre);
-    nouvCarre.addEventListener("click", clique());
+    let nouvCarre = carre.cloneNode();// création d'un carré
+    nouvCarre.innerText = i;// avec un chiffre
+    contenu.appendChild(nouvCarre);// on l'ajoute au conteneur
+    nouvCarre.addEventListener("click", function(){ // quand on clique ce carré :
+        if(nouvCarre.classList.contains("tourne")){ // soit il a déjà tourner
+            removeClass(nouvCarre, "tourne");// donc on enlève la classe "tourne"
+        }
+        else{ // soit il n'a pas encore tourner
+            addClass(nouvCarre, "tourne");// donc on le fait tourner
+        }
+    });
 }
