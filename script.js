f726a8216d735f2b36989c49db332a477f9cafc8
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-03-28 13:47:27 +0200

    Ajustements

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-/* Fonction pour ajouter une classe à un élément */
-function addClass(element, classe){
-    element.classList.add(classe);
-}
-
-/* Fonction pour enlever une classe à un élément */
-function removeClass(element, classe){
-    element.classList.remove(classe);
-}
-
 /* Création d'un carré */
 const carre = document.createElement("div");
 carre.classList.add("carre");
@@ -21,12 +11,12 @@ for(i = 1; i <= 4; i++){
     contenu.appendChild(nouvCarre);// on l'ajoute au conteneur
     nouvCarre.addEventListener("click", function(){ // quand on clique ce carré :
         if(nouvCarre.classList.contains("tourne")){ // soit il a déjà tourner
-            removeClass(nouvCarre, "tourne");// donc on enlève la classe "tourne"
-            addClass(nouvCarre, "base");
+            nouvCarre.classList.toggle("tourne");// donc on enlève la classe "tourne"
+            nouvCarre.classList.toggle("base");
         }
         else{ // soit il n'a pas encore tourner
-            removeClass(nouvCarre, "base");
-            addClass(nouvCarre, "tourne");// donc on le fait tourner
+            nouvCarre.classList.toggle("base");
+            nouvCarre.classList.toggle("tourne");
         }
     });
 }
